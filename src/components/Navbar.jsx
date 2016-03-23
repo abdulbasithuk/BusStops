f856f8986ed63ee20891af7734d4c9e97257da46
Abdul <abdulbasithuk@gmail.com>
2016-03-23 22:44:21 +0000

    Navbar style and Font-awesome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,10 @@ var Navbar = React.createClass({
       <div className="navbar-top">
         <div className="container">
           <img className="cd-profile-image" src="/images/profile_image.jpg" alt="profile" />
+          <div className="cd-social-connect">
+            <i className="fa fa-github"></i>
+            <i className="fa fa-linkedin-square"></i>
+          </div>
         </div>
       </div>
     );
