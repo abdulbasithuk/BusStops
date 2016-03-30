84bdac7da909cf0c3135a147e2cedb379d866de4
Abdul <abdulbasithuk@gmail.com>
2016-03-30 19:59:27 +0100

    Basic app layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,8 +17,20 @@ var HomePage = React.createClass({
   },
   render() {
     return (
-      <div>
-        <Searchbox />
+      <div className="text-center">
+        <div className="row">
+          <div class="col-md-6 col-md-offset-3">
+            <Searchbox />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-8 col-sm-6">
+            Nearby bus stops info
+          </div>
+          <div className="col-xs-4 col-sm-6">
+            Google map with pointers
+          </div>
+        </div>
       </div>
     );
   }
