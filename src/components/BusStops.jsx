90dcd8459b37c70e9817d8e1a0817097f9ee96bc
Abdul <abdulbasithuk@gmail.com>
2016-04-05 21:02:58 +0100

    Bus stop component styling

--- src/components/BusStops.jsx
+++ src/components/BusStops.jsx
@@ -8,8 +8,31 @@ var React = require('react');
 var BusStops = React.createClass({
   render() {
     return (
-      <div className="mod-bus-stops-container">
-        Nearby bus stops info
+      <div className="com-bus-stops-container">
+        <div className="cd-heading">
+          <i className="fa fa-bus"></i>
+          <span className="cd-title">Bus Stops</span>
+        </div>
+        <table className="cd-placeholder">
+          <tr className="cd-item">
+            <td className="cd-icon"><i className="fa fa-map-marker"></i></td>
+            <td>
+              <span className="cd-text"></span>
+            </td>
+          </tr>
+          <tr className="cd-item">
+            <td className="cd-icon"><i className="fa fa-map-marker"></i></td>
+            <td>
+              <span className="cd-text"></span>
+            </td>
+          </tr>
+          <tr className="cd-item">
+            <td className="cd-icon"><i className="fa fa-map-marker"></i></td>
+            <td>
+              <span className="cd-text"></span>
+            </td>
+          </tr>
+        </table>
       </div>
     );
   }
