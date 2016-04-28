f0a1aa935a2b00f81d6f92da1e0a728909fcca64
Abdul <abdulbasithuk@gmail.com>
2016-04-28 22:58:16 +0100

    Re-initialised Firebase & published the app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 require('../styles/navbar.less');
 import React from 'react';
 
+let profileImage = require('../images/profile_image.jpg');
+
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar-top">
         <div className="container">
-          <img className="cd-profile-image" src="/images/profile_image.jpg" alt="profile" />
+          <img className="cd-profile-image" src={profileImage}/>
           <div className="cd-social-connect">
             <i className="fa fa-github"></i>
             <i className="fa fa-linkedin-square"></i>
